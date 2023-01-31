cbc1e8729182422f63c7482322be808900f26147
oleg_baulin <oleg.letit@gmail.com>
2023-01-31 15:42:59 +0300

    8.4 практическая работа

---
+++ 08_api_2/src/components/OrderList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul class="cart__orders">
+      <li class="cart__order" v-for="product in products" :key="product.productId">
+        <h3>{{ product.product.title }}</h3>
+        <b>{{ product.product.price | numberFormat}}</b>
+        <span>Артикул: {{ product.productId }}</span>
+      </li>
+    </ul>
+
+    <div class="cart__total">
+      <p>Доставка: <b>500 ₽</b></p>
+      <p>Итого: <b>{{ totalProduct }}</b> {{ totalProduct | endingsFormat }} на сумму <b>{{ totalPrice | numberFormat}}</b></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import numberFormat from '@/helpers/numberFormat';
+import endingsFormat from '@/helpers/endingsFormat';
+
+export default {
+  props: ['products', 'totalProduct', 'totalPrice'],
+  filters: {
+    numberFormat,
+    endingsFormat,
+  },
+};
+</script>
---
+++ 08_api_2/src/pages/CartPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html">
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            Корзина
+          </a>
+        </li>
+      </ul>
+
+      <h1 class="content__title">
+        Корзина
+      </h1>
+      <span class="content__info">
+        {{ totalProduct }} {{ totalProduct | endingsFormat }}
+      </span>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST">
+        <Preloader v-if="$store.state.cartProductsLoading" />
+        <div v-else class="cart__field">
+          <ul class="cart__list">
+            <CartItem v-for="item in products" :key="item.productId" :item="item" />
+          </ul>
+        </div>
+
+        <div class="cart__block">
+          <p class="cart__desc">
+            Мы&nbsp;посчитаем стоимость доставки на&nbsp;следующем этапе
+          </p>
+          <p class="cart__price">
+            Итого: <span>{{ totalPrice | numberFormat }} ₽</span>
+          </p>
+
+          <router-link v-if="totalProduct" tag="button" :to="{ name: 'order' }" class="cart__button button button--primery" type="submit">
+            Оформить заказ
+          </router-link>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script>
+import numberFormat from '@/helpers/numberFormat';
+import endingsFormat from '@/helpers/endingsFormat';
+import { mapGetters } from 'vuex';
+import CartItem from '@/components/CartItem.vue';
+import Preloader from '@/components/Preloader.vue';
+
+export default {
+  filters: {
+    numberFormat,
+    endingsFormat,
+  },
+  components: {
+    CartItem,
+    Preloader,
+  },
+  computed: {
+    ...mapGetters({
+      products: 'cartDetailProducts',
+      totalPrice: 'cartTotalPrice',
+      totalProduct: 'cartTotalProduct',
+    }),
+  },
+};
+</script>
